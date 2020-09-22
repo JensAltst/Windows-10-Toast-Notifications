883569bd626927b6dff5b816a11a827bc979a3e0
Daturo <49022685+JensAltst@users.noreply.github.com>
2020-09-22 15:08:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@
 from operator import attrgetter
 from os import path
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 from setuptools import setup
 
 def read(fname):
