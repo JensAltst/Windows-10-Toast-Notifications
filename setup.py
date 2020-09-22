d26a495e2bfbae14b324fb9b3770c11569025a6f
Daturo <49022685+JensAltst@users.noreply.github.com>
2020-09-22 15:13:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,13 +15,6 @@ def parse_requirements(filename):
     lines = (line.strip() for line in open(filename))
     return [line for line in lines if line and not line.startswith("#")]
 
-
-with open(path.join(BASE_DIR, "README.rst")) as readme_file:
-    readme = readme_file.read()
-
-with open(path.join(BASE_DIR, "HISTORY.rst")) as history_file:
-    history = history_file.read()
-
 req_files = {
     "requirements": "requirements.txt",
 }
