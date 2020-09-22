6c6097963084494c611076b4a586f083a0d1f304
Daturo <49022685+JensAltst@users.noreply.github.com>
2020-09-22 15:12:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
 
-from operator import attrgetter
+"""The setup script."""
+
 from os import path
 
-from pip._internal.req import parse_requirements
-from setuptools import setup
+from setuptools import find_packages, setup
 
-def read(fname):
-    return open(path.join(path.dirname(__file__), fname)).read()
+import versioneer
 
+BASE_DIR = path.abspath(path.dirname(__file__))
 
-def from_here(relative_path):
-    return path.join(path.dirname(__file__), relative_path)
 
 def parse_requirements(filename):
     """ load requirements from a pip requirements file """
     lines = (line.strip() for line in open(filename))
     return [line for line in lines if line and not line.startswith("#")]
 
-requirements_txt = list(map(str, map(
-    attrgetter("req"),
-    parse_requirements(from_here("requirements.txt"))
-)))
+
+with open(path.join(BASE_DIR, "README.rst")) as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(BASE_DIR, "HISTORY.rst")) as history_file:
+    history = history_file.read()
+
+req_files = {
+    "requirements": "requirements.txt",
+}
+
+requirements = {}
+for req, req_file in req_files.items():
+    requirements[req] = parse_requirements(req_file)
 
 setup(
     name="win10toast",
